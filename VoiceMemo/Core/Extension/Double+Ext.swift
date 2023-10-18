556b6f2e346fa3f7d20baab3d6b38aca092f6063
YuGeonHui <ghyu_0906@naver.com>
2023-10-18 17:24:56 +0900

    음성 녹음 화면 개발완료

---
+++ VoiceMemo/Core/Extension/Double+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Double+Ext.swift
+//  VoiceMemo
+//
+//  Created by geonhui Yu on 10/18/23.
+//
+
+import Foundation
---
+++ VoiceMemo/Feature/Home/VocieRecorder/VoiceRecorderView.swift
@@ -0,0 +1,18 @@
+//
+//  VoiceRecorderView.swift
+//  VoiceMemo
+//
+//  Created by geonhui Yu on 10/18/23.
+//
+
+import SwiftUI
+
+struct VoiceRecorderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VoiceRecorderView()
+}
